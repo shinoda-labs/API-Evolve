0710f2eb38a15753fe7501aa1e65e3c827e1d9ba
Rodrigo <rodrigo.viegas@ulbra.inf.br>
2020-05-30 00:53:33 -0300

    Limit do Json e CORS

--- src/app.js
+++ src/app.js
@@ -19,8 +19,14 @@ const userRoute = require('./routes/user')
 const authorizeRoute = require('./routes/authorize')
 const postRoute = require('./routes/post')
 
-app.use(bodyParser.json())
+app.use(bodyParser.json({ limit: '5mb' }))
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    next()
+})
 
 app.use('/', indexRoute)
 app.use('/authorize', authorizeRoute)
